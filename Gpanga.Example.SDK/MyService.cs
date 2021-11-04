610ae08f8e3a11189990c73b1a869b63e1586e2d
Gabriel Pan Gantes <gabrielpanga@gmail.com>
2021-11-04 09:03:04 -0300

    feat: add sample project to test actions

---
+++ Gpanga.Example.SDK/MyService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Gpanga.Example.SDK
+{
+    public class MyService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Gpanga.Example.Tests/MyService.cs
@@ -0,0 +1,15 @@
+﻿using Gpanga.Example.SDK;
+using NUnit.Framework;
+
+namespace Gpanga.Example.Tests
+{
+    public class Tests
+    {
+
+        [Test]
+        public void MyService_SumTwoNumbers()
+        {
+            Assert.AreEqual(MyService.Sum(2, 4), 6);
+        }
+    }
+}
